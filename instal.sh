6e203ac6d90414310c2bf41a0d74e032e938b516
Selena <selena.7.stojkovic@gmail.com>
2023-12-29 00:28:10 +0100

    copy xorg touchpad config

--- instal.sh
+++ instal.sh
@@ -9,5 +9,7 @@ ln -s "$DOTFILES_DIR/.config" "$HOME/.config"
 #dodaj za xorg touchpad i xresources
 # u .Xresources samo stavi da je Xft 96, ovo bi terabalo da je pwd /etc/X11/xinit/.Xresources
 
+sudo cp "$DOTFILES_DIR/etc/xorg.conf.d/30-touchpad.conf" "/etc/X11/xorg.conf.d/"
+
 chsh -s $(which fish)
 
