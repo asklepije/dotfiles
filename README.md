e614781d5ae58e108aaba63e18db41c033207225
Selena <selena.7.stojkovic@gmail.com>
2023-12-29 00:16:21 +0100

    initial setup

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # dotfiles
+## enable networkmanager
+sudo systemctl enable NetworkManager
+
+sudo systemctl start NetworkManager
+
+## skini yay
+cd /otp
+
+git clone https://aur.archlinux.org/yay.git
+
+sudo chown -R asklepijes:asklepijes yay
+
+cd yay
+
+makepkg -si
+
+##Xresources bitno i xinitrc oni su u home folderu
+
+napravi linkove za x11
---
+++ instal.sh
@@ -0,0 +1,10 @@
+sudo pacman -Syu
+
+sudo yay -S xf86-video-intel xorg-server xorg-xinit xf86-input-libinput fish alacritty nano bspwm picom code flameshot rofi polybar
+
+DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/dotfiles"
+
+ln -s "$DOTFILES_DIR/.config" "$HOME/.config"
+
+#dodaj za xorg touchpad i xresources
+# u .Xresources samo stavi da je Xft 96, ovo bi terabalo da je pwd /etc/X11/xinit/.Xresources
