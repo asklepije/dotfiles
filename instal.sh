41d06c80baac2c2064170d8c16d52a2ae55fe820
Selena <selena.7.stojkovic@gmail.com>
2024-01-02 16:29:13 +0100

    Add xinitrc

--- instal.sh
+++ instal.sh
@@ -5,22 +5,24 @@ sudo yay -S xf86-video-intel xorg-server xorg-xinit xf86-input-libinput fish ala
 DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 
-dirs=$(find "$DOTFILES_DIR"/.config -mindepth 1 -type d -printf "%f\n")
+# dirs=$(find "$DOTFILES_DIR"/.config -mindepth 1 -type d -printf "%f\n")
+dirs=$(find "$DOTFILES_DIR"/.config -mindepth 1 -type d -exec basename {} \;)
 
 
-for dir in $dirs; do
+# for dir in $dirs; do
+echo "$dirs"|while IFS= read -r dir; do
     ln -s "$DOTFILES_DIR/.config/$dir" "$HOME/.config/$dir" 
 done
 
 mkdir $HOME/Pictures
 sudo cp "$DOTFILES_DIR/wallpaper.png" "$HOME/Pictures/wallpaper.png"
 
-sudo cp /etc/X11/xinit/xinitrc $HOME/.xinitrc
-
 #dodaj za xorg touchpad i xresources
 # u .Xresources samo stavi da je Xft 96, ovo bi terabalo da je pwd /etc/X11/xinit/.Xresources
+#fali ti code config
 
 ln -s "$DOTFILES_DIR/.Xresources" "$HOME/.Xresources" 
+ln -s "$DOTFILES_DIR/.xinitrc" "$HOME/.xinitrc" 
 
 sudo cp "$DOTFILES_DIR/etc/xorg.conf.d/30-touchpad.conf" "/etc/X11/xorg.conf.d/"
 
