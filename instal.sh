39708f6eb002f2c2920480f7d767548b675b73af
Selena <selena.7.stojkovic@gmail.com>
2024-01-02 16:24:43 +0100

    Add Xresources

--- instal.sh
+++ instal.sh
@@ -4,24 +4,12 @@ sudo yay -S xf86-video-intel xorg-server xorg-xinit xf86-input-libinput fish ala
 
 DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-#ln -s "$DOTFILES_DIR/.config"/* "$HOME/.config"
+
 dirs=$(find "$DOTFILES_DIR"/.config -mindepth 1 -type d -printf "%f\n")
-echo $dirs
-# Loop through list
+
+
 for dir in $dirs; do
-
-  # Only run on directories
-
-    # # Get directory name 
-    # dir_name="$(basename "$dir")"
-
-    # Copy directory
-    # cp -r "$DOTFILES_DIR/.config/$dir" "$HOME/.config/$dir"
-
-    # Create symlink to copied directory
     ln -s "$DOTFILES_DIR/.config/$dir" "$HOME/.config/$dir" 
-
-
 done
 
 mkdir $HOME/Pictures
@@ -31,6 +19,8 @@ sudo cp /etc/X11/xinit/xinitrc $HOME/.xinitrc
 
 #dodaj za xorg touchpad i xresources
 # u .Xresources samo stavi da je Xft 96, ovo bi terabalo da je pwd /etc/X11/xinit/.Xresources
+
+ln -s "$DOTFILES_DIR/.Xresources" "$HOME/.Xresources" 
 
 sudo cp "$DOTFILES_DIR/etc/xorg.conf.d/30-touchpad.conf" "/etc/X11/xorg.conf.d/"
 
